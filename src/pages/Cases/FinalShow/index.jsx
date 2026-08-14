29d83a42329658f8926ab5abcab88b5557f21e83
keenBank <107559971+keenBank@users.noreply.github.com>
2026-08-14 17:27:09 +0300

    Add welcomeUrl and fake-numbers notice to Final Show

--- src/pages/Cases/FinalShow/index.jsx
+++ src/pages/Cases/FinalShow/index.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import CaseHome from "../../../components/layout/CaseHome";
+import FinalShowNotice from "./FinalShowNotice";
 import thumbnail from "../../../assets/home/images/cases/last.png";
 
 export default function FinalShowHome() {
   return (
-    <CaseHome
-      slug="final-show"
-      thumbnail={thumbnail}
-      title="العرض الأخير"
-      description={
-        'من فوق خشبة المسرح وأمام مرأى الجميع خطفت الطفلة نايا ليبقى الدليل الوحيد قناعاً مبتسم وصرخات الطفلة،، بين يديك قضية مختلفة في هذه القضية أراد المجرم أن يظهر للعلن لا أن يختفي داخل ملفات التحقيق'
-      }
-    />
+    <>
+      <CaseHome
+        slug="final-show"
+        thumbnail={thumbnail}
+        title="العرض الأخير"
+        welcomeUrl="https://news-nine-red.vercel.app/"
+        description={
+          'من فوق خشبة المسرح وأمام مرأى الجميع خطفت الطفلة نايا ليبقى الدليل الوحيد قناعاً مبتسم وصرخات الطفلة،، بين يديك قضية مختلفة في هذه القضية أراد المجرم أن يظهر للعلن لا أن يختفي داخل ملفات التحقيق'
+        }
+      />
+      <FinalShowNotice />
+    </>
   );
 }
