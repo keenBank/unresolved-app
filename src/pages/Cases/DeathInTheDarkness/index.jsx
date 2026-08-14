9cc128a3f845f46342c69d0c322eee1328549d8c
keenBank <107559971+keenBank@users.noreply.github.com>
2026-08-14 17:20:23 +0300

    Add welcomeUrl for Death in the Darkness case

--- src/pages/Cases/DeathInTheDarkness/index.jsx
+++ src/pages/Cases/DeathInTheDarkness/index.jsx
@@ -8,6 +8,7 @@ export default function DeathInTheDarknessHome() {
       slug="death-in-the-darkness"
       thumbnail={thumbnail}
       title="موت في الظلام"
+      welcomeUrl="https://death-in-the-darkness.vercel.app/"
       description={
         'تروي القصة حادثة انتحار وقعت في أحد المنازل المرموقة راح ضحيتها شاب يدعى رامي، لكن القصة لم تنته بعد؟ ما سر البيت المظلم ولماذا كتب رامي قبل أن يموت " أقرب من ظلك أبعد من الشمس" لم يستطع أحد من المحققين أن يفك رموز الحادثة لذا تم اغلاق القضية في حينها. نريد منك اعادة التحقيق مرة أخرى والكشف عن المجرم'
       }
